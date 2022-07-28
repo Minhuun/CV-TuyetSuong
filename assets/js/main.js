e7ab164f72323c14decfe58d246cc13b60ad8134
Ly Minh Hung <hunglm@devcamp.com>
2022-07-28 22:20:36 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($) {
+
+
+	var mastheadheight = $('.ds-header').outerHeight();
+	//console.log(mastheadheight);
+	$(".ds-banner,.ds-main-section").css("margin-top" , mastheadheight);
+
+	$(window).scroll(function(){
+	    if ($(window).scrollTop() >= 10) {
+	        $('.ds-header').addClass('ds-fixed-header');
+	    }
+	    else {
+	        $('.ds-header').removeClass('ds-fixed-header');
+	    }
+	}).scroll();
+
+	$("#btn-detail").click(function(){
+		$("#ul-detail").show(function(){
+			$("#btn-detail").click(function(){
+				$("#ul-detail").hide()
+			});
+		});
+	})
+
+	$("#btn-detail2").click(function(){
+		$("#ul-detail2").show(function(){
+			$("#btn-detail2").click(function(){
+				$("#ul-detail2").hide()
+			});
+		});
+	})
+
+
+});
